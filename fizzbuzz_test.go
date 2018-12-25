06f9357f0b855b16e99a8cf83e4be1961452e793
Porjed Kuljittiyanee - TTH-Dev <p.kuljittiyanee@togetherteam.co.th>
2018-12-25 17:40:12 +0700

    Refactor fizzbuzz_test with Subtests

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -2,39 +2,42 @@ package fizzbuzz
 
 import "testing"
 
-func assertCorrectMessage (t *testing.T, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+func TestFizzBuzz (t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		} 
 	}
-}
 
-func TestFizzBuzz1Return1(t *testing.T) {
-	got := FizzBuzz(1)
-	want := "1"
-	assertCorrectMessage(t, got, want)
-}
+	t.Run("Test FizzBuzz 1 Return 1", func(t *testing.T) {
+		got := FizzBuzz(1)
+		want := "1"
+		assertCorrectMessage(t, got, want)
+	})
 
-func TestFizzBuzz2Return2(t *testing.T) {
-	got := FizzBuzz(2)
-	want := "2"
-	assertCorrectMessage(t, got, want)
-}
+	t.Run("Test FizzBuzz 2 Return 2", func(t *testing.T) {
+		got := FizzBuzz(2)
+		want := "2"
+		assertCorrectMessage(t, got, want)
+	})
 
-func TestFizzBuzz3ReturnFizz(t *testing.T) {
-	got := FizzBuzz(3)
-	want := "Fizz"
-	assertCorrectMessage(t, got, want)
-}
+	t.Run("Test FizzBuzz 3 Return Fizz", func(t *testing.T) {
+		got := FizzBuzz(3)
+		want := "Fizz"
+		assertCorrectMessage(t, got, want)
+	})
 
-func TestFizzBuzz5ReturnBuzz(t *testing.T) {
-	got := FizzBuzz(5)
-	want := "Buzz"
-	assertCorrectMessage(t, got, want)
-}
-
-func TestFizzBuzz15ReturnFizzBuzz(t *testing.T) {
-	got := FizzBuzz(15)
-	want := "FizzBuzz"
-	assertCorrectMessage(t, got, want)
+	t.Run("Test FizzBuzz 5 Return Buzz", func(t *testing.T) {
+		got := FizzBuzz(5)
+		want := "Buzz"
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("Test FizzBuzz 15 Return FizzBuzz", func(t *testing.T) {
+		got := FizzBuzz(15)
+		want := "FizzBuzz"
+		assertCorrectMessage(t, got, want)
+	})	
 }
