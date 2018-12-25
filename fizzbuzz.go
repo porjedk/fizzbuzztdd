68a33aa91edce36ff3bbb6f4dbd161106c58e34e
Porjed Kuljittiyanee - TTH-Dev <p.kuljittiyanee@togetherteam.co.th>
2018-12-25 18:09:16 +0700

    Refactor FizzBuzz function with switch case.

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -6,15 +6,14 @@ func FizzBuzz(got int) string {
 
 	var want string = ""
 
-	if got%3 == 0 {
+	switch {
+	case (got%3 == 0) && (got%5 == 0):
+		want = "FizzBuzz"
+	case got%3 == 0:
 		want = "Fizz"
-	}
-
-	if got%5 == 0 {
-		want += "Buzz"
-	}
-
-	if want == "" {
+	case got%5 == 0:
+		want = "Buzz"
+	default:
 		want = strconv.Itoa(got)
 	}
 
